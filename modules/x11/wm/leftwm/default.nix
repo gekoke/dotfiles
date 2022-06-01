8c120f7c8a7b81a39ffc4052f2ebc8b512558c01
gekoke <gekoke@lazycantina.xyz>
2022-06-01 21:17:09 +0300

    Fix leftwm theme

--- modules/x11/wm/leftwm/default.nix
+++ modules/x11/wm/leftwm/default.nix
@@ -1,11 +1,15 @@
-{ ... }:
+{ pkgs, ... }:
 let
-  themeFile = ./theme.toml;
-in {
+  theme = "spacejelly";
+in
+{
   imports =
     [
       ./home.nix
     ];
 
    xdg.configFile."leftwm/".source = ./config;
+   xsession.initExtra = ''
+     ${pkgs.leftwm}/bin/leftwm-command "LoadTheme ~/.config/leftwm/${theme}/theme.toml"
+   '';
 }
