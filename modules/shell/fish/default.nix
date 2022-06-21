7ca1d9d46a6f807f37cb6817cac8535d80e31d7b
gekoke <gekoke@lazycantina.xyz>
2022-06-21 22:58:02 +0300

    Add greeting for fish shell

--- modules/shell/fish/default.nix
+++ modules/shell/fish/default.nix
@@ -1,5 +1,15 @@
-{ lib, ... }:
+{ lib, pkgs, ... }:
 {
+  home.packages = with pkgs;
+  [
+    lolcat
+    neofetch
+    boxes
+    fortune
+  ];
+
+  xdg.configFile."fish/conf.d/".source = ./config/conf.d;
+
   programs = {
     fzf = {
       enable = true;
@@ -17,6 +27,5 @@
 
   home.sessionVariables = {
     fish_prompt_pwd_dir_length = 0;
-    fish_greeting = "";
   };
 }
