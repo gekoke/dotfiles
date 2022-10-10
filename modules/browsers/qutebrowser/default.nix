fdb18869d660334b7bc29e21d3044dcd1f9c4cff
gekoke <gekoke@lazycantina.xyz>
2022-10-10 19:23:36 +0300

    Theme qutebrowser dynamically with Nix config

--- modules/browsers/qutebrowser/default.nix
+++ modules/browsers/qutebrowser/default.nix
@@ -6,6 +6,7 @@
 with lib; let
   cfg = config.modules.browsers.qutebrowser;
   pythonDependencies = python-packages: with python-packages; [ adblock ];
+  paletteAsJson = builtins.toJSON config.prefs.style.palette;
 in
 {
   options.modules.browsers.qutebrowser = {
@@ -15,7 +16,12 @@ in
   config = mkIf cfg.enable {
     programs.qutebrowser = {
       enable = true;
-      extraConfig = builtins.readFile ./config.py;
+      extraConfig = ''
+        ${builtins.readFile ./config.py}
+        import json
+        theme = ${paletteAsJson} # Also a valid Python dictionary
+        ${builtins.readFile ./theme.py}
+      '';
     };
     home.packages = with pkgs; [
       (python38.withPackages pythonDependencies)
---
+++ modules/browsers/qutebrowser/theme.py
@@ -0,0 +1,308 @@
+## Background color of the completion widget category headers.
+## Type: QssColor
+c.colors.completion.category.bg = theme['base0']
+
+## Bottom border color of the completion widget category headers.
+## Type: QssColor
+c.colors.completion.category.border.bottom = theme['base0']
+
+## Top border color of the completion widget category headers.
+## Type: QssColor
+c.colors.completion.category.border.top = theme['base0']
+
+## Foreground color of completion widget category headers.
+## Type: QtColor
+c.colors.completion.category.fg = theme['lite0']
+
+## Background color of the completion widget for even rows.
+## Type: QssColor
+c.colors.completion.even.bg = theme['base1']
+
+## Background color of the completion widget for odd rows.
+## Type: QssColor
+c.colors.completion.odd.bg = theme['base1']
+
+## Text color of the completion widget.
+## Type: QtColor
+c.colors.completion.fg = theme['lite0']
+
+## Background color of the selected completion item.
+## Type: QssColor
+c.colors.completion.item.selected.bg = theme['base3']
+
+## Bottom border color of the selected completion item.
+## Type: QssColor
+c.colors.completion.item.selected.border.bottom = theme['base3']
+
+## Top border color of the completion widget category headers.
+## Type: QssColor
+c.colors.completion.item.selected.border.top = theme['base3']
+
+## Foreground color of the selected completion item.
+## Type: QtColor
+c.colors.completion.item.selected.fg = theme['lite1']
+
+## Foreground color of the matched text in the completion.
+## Type: QssColor
+c.colors.completion.match.fg = theme['colo2']
+
+## Color of the scrollbar in completion view
+## Type: QssColor
+c.colors.completion.scrollbar.bg = theme['base1']
+
+## Color of the scrollbar handle in completion view.
+## Type: QssColor
+c.colors.completion.scrollbar.fg = theme['lite0']
+
+## Background color for the download bar.
+## Type: QssColor
+c.colors.downloads.bar.bg = theme['base0']
+
+## Background color for downloads with errors.
+## Type: QtColor
+c.colors.downloads.error.bg = theme['colo0']
+
+## Foreground color for downloads with errors.
+## Type: QtColor
+c.colors.downloads.error.fg = theme['lite0']
+
+## Color gradient stop for download backgrounds.
+## Type: QtColor
+c.colors.downloads.stop.bg = theme['colo3']
+
+## Color gradient interpolation system for download backgrounds.
+## Type: ColorSystem
+## Valid values:
+##   - rgb: Interpolate in the RGB color system.
+##   - hsv: Interpolate in the HSV color system.
+##   - hsl: Interpolate in the HSL color system.
+##   - none: Don't show a gradient.
+c.colors.downloads.system.bg = 'none'
+
+## Background color for hints. Note that you can use a `rgba(...)` value
+## for transparency.
+## Type: QssColor
+c.colors.hints.bg = theme['colo2']
+
+## Font color for hints.
+## Type: QssColor
+c.colors.hints.fg = theme['base0']
+
+## Font color for the matched part of hints.
+## Type: QssColor
+c.colors.hints.match.fg = theme['colo3']
+
+## Background color of the keyhint widget.
+## Type: QssColor
+c.colors.keyhint.bg = theme['base1']
+
+## Text color for the keyhint widget.
+## Type: QssColor
+c.colors.keyhint.fg = theme['lite0']
+
+## Highlight color for keys to complete the current keychain.
+## Type: QssColor
+c.colors.keyhint.suffix.fg = theme['colo2']
+
+## Background color of an error message.
+## Type: QssColor
+c.colors.messages.error.bg = theme['colo0']
+
+## Border color of an error message.
+## Type: QssColor
+c.colors.messages.error.border = theme['colo0']
+
+## Foreground color of an error message.
+## Type: QssColor
+c.colors.messages.error.fg = theme['lite0']
+
+## Background color of an info message.
+## Type: QssColor
+c.colors.messages.info.bg = theme['colo1']
+
+## Border color of an info message.
+## Type: QssColor
+c.colors.messages.info.border = theme['colo1']
+
+## Foreground color an info message.
+## Type: QssColor
+c.colors.messages.info.fg = theme['lite0']
+
+## Background color of a warning message.
+## Type: QssColor
+c.colors.messages.warning.bg = theme['colo1']
+
+## Border color of a warning message.
+## Type: QssColor
+c.colors.messages.warning.border = theme['colo1']
+
+## Foreground color a warning message.
+## Type: QssColor
+c.colors.messages.warning.fg = theme['lite0']
+
+## Background color for prompts.
+## Type: QssColor
+c.colors.prompts.bg = theme['base2']
+
+# ## Border used around UI elements in prompts.
+# ## Type: String
+c.colors.prompts.border = '1px solid ' + theme['base0']
+
+## Foreground color for prompts.
+## Type: QssColor
+c.colors.prompts.fg = theme['lite0']
+
+## Background color for the selected item in filename prompts.
+## Type: QssColor
+c.colors.prompts.selected.bg = theme['base3']
+
+## Background color of the statusbar in caret mode.
+## Type: QssColor
+c.colors.statusbar.caret.bg = theme['colo3']
+
+## Foreground color of the statusbar in caret mode.
+## Type: QssColor
+c.colors.statusbar.caret.fg = theme['lite0']
+
+## Background color of the statusbar in caret mode with a selection.
+## Type: QssColor
+c.colors.statusbar.caret.selection.bg = theme['colo3']
+
+## Foreground color of the statusbar in caret mode with a selection.
+## Type: QssColor
+c.colors.statusbar.caret.selection.fg = theme['lite0']
+
+## Background color of the statusbar in command mode.
+## Type: QssColor
+c.colors.statusbar.command.bg = theme['base2']
+
+## Foreground color of the statusbar in command mode.
+## Type: QssColor
+c.colors.statusbar.command.fg = theme['lite0']
+
+## Background color of the statusbar in private browsing + command mode.
+## Type: QssColor
+c.colors.statusbar.command.private.bg = theme['base2']
+
+## Foreground color of the statusbar in private browsing + command mode.
+## Type: QssColor
+c.colors.statusbar.command.private.fg = theme['lite0']
+
+## Background color of the statusbar in insert mode.
+## Type: QssColor
+c.colors.statusbar.insert.bg = theme['colo3']
+
+## Foreground color of the statusbar in insert mode.
+## Type: QssColor
+c.colors.statusbar.insert.fg = theme['base1']
+
+## Background color of the statusbar.
+## Type: QssColor
+c.colors.statusbar.normal.bg = theme['base0']
+
+## Foreground color of the statusbar.
+## Type: QssColor
+c.colors.statusbar.normal.fg = theme['lite0']
+
+## Background color of the statusbar in passthrough mode.
+## Type: QssColor
+c.colors.statusbar.passthrough.bg = theme['colo3']
+
+## Foreground color of the statusbar in passthrough mode.
+## Type: QssColor
+c.colors.statusbar.passthrough.fg = theme['lite0']
+
+## Background color of the statusbar in private browsing mode.
+## Type: QssColor
+c.colors.statusbar.private.bg = theme['base3']
+
+## Foreground color of the statusbar in private browsing mode.
+## Type: QssColor
+c.colors.statusbar.private.fg = theme['lite0']
+
+## Background color of the progress bar.
+## Type: QssColor
+c.colors.statusbar.progress.bg = theme['lite0']
+
+## Foreground color of the URL in the statusbar on error.
+## Type: QssColor
+c.colors.statusbar.url.error.fg = theme['colo0']
+
+## Default foreground color of the URL in the statusbar.
+## Type: QssColor
+c.colors.statusbar.url.fg = theme['lite0']
+
+## Foreground color of the URL in the statusbar for hovered links.
+## Type: QssColor
+c.colors.statusbar.url.hover.fg = theme['colo1']
+
+## Foreground color of the URL in the statusbar on successful load
+## (http).
+## Type: QssColor
+c.colors.statusbar.url.success.http.fg = theme['lite0']
+
+## Foreground color of the URL in the statusbar on successful load
+## (https).
+## Type: QssColor
+c.colors.statusbar.url.success.https.fg = theme['colo3']
+
+## Foreground color of the URL in the statusbar when there's a warning.
+## Type: QssColor
+c.colors.statusbar.url.warn.fg = theme['colo1']
+
+## Background color of the tab bar.
+## Type: QtColor
+c.colors.tabs.bar.bg = theme['base3']
+
+## Background color of unselected even tabs.
+## Type: QtColor
+c.colors.tabs.even.bg = theme['base3']
+
+## Foreground color of unselected even tabs.
+## Type: QtColor
+c.colors.tabs.even.fg = theme['lite0']
+
+## Color for the tab indicator on errors.
+## Type: QtColor
+c.colors.tabs.indicator.error = theme['colo0']
+
+## Color gradient start for the tab indicator.
+## Type: QtColor
+# c.colors.tabs.indicator.start = theme['violet']
+
+## Color gradient end for the tab indicator.
+## Type: QtColor
+# c.colors.tabs.indicator.stop = theme['orange']
+
+## Color gradient interpolation system for the tab indicator.
+## Type: ColorSystem
+## Valid values:
+##   - rgb: Interpolate in the RGB color system.
+##   - hsv: Interpolate in the HSV color system.
+##   - hsl: Interpolate in the HSL color system.
+##   - none: Don't show a gradient.
+c.colors.tabs.indicator.system = 'none'
+
+## Background color of unselected odd tabs.
+## Type: QtColor
+c.colors.tabs.odd.bg = theme['base3']
+
+## Foreground color of unselected odd tabs.
+## Type: QtColor
+c.colors.tabs.odd.fg = theme['lite0']
+
+# ## Background color of selected even tabs.
+# ## Type: QtColor
+c.colors.tabs.selected.even.bg = theme['base0']
+
+# ## Foreground color of selected even tabs.
+# ## Type: QtColor
+c.colors.tabs.selected.even.fg = theme['lite0']
+
+# ## Background color of selected odd tabs.
+# ## Type: QtColor
+c.colors.tabs.selected.odd.bg = theme['base0']
+
+# ## Foreground color of selected odd tabs.
+# ## Type: QtColor
+c.colors.tabs.selected.odd.fg = theme['lite0']
