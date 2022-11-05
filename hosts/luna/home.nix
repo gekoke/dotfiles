549a846164283aa521376c8f983a08df5f2c9556
gekoke <gekoke@lazycantina.xyz>
2022-11-05 21:11:35 +0200

    Add pywal program

--- hosts/luna/home.nix
+++ hosts/luna/home.nix
@@ -3,107 +3,115 @@
 , pkgs
 , user
 , ...
-}: {
-  config = {
-    nixpkgs.config.allowUnfree = true;
+}:
+with lib;
+{
+  config = mkMerge [
+    {
+      home.packages = with pkgs; [ python2 ];
+      programs.pywal.enable = true;
+    }
+    {
+      nixpkgs.config.allowUnfree = true;
 
-    home = {
-      username = "${user}";
-      homeDirectory = "/home/${user}";
-      stateVersion = "22.05";
+      home = {
+        username = "${user}";
+        homeDirectory = "/home/${user}";
+        stateVersion = "22.05";
 
-      keyboard = {
-        layout = "ee";
-        variant = "us";
-      };
-
-      packages = with pkgs; [
-        bitwarden
-        neofetch
-        coreutils
-        tldr
-        spotify
-        discord
-        teamspeak_client
-        signal-desktop
-        element-desktop
-      ];
-    };
-
-    programs = {
-      home-manager.enable = true;
-      git = {
-        userName = "gekoke";
-        userEmail = "gekoke@lazycantina.xyz";
-      };
-      mpv.enable = true;
-      rtorrent.enable = true;
-    };
-
-    services = {
-      unclutter = {
-        enable = true;
-        timeout = 5;
-      };
-    };
-
-    modules = {
-      windowmanagers.awesome.enable = true;
-
-      dev = {
-        nix.enable = true;
-        c.enable = true;
-        prolog.enable = true;
-        haskell.enable = true;
-        python.enable = true;
-        lua.enable = true;
-      };
-
-      editors = {
-        enable = true;
-        default = "nvim";
-        neovim = {
-          enable = true;
-          enableNix = true;
-          colorscheme = "nord";
+        keyboard = {
+          layout = "ee";
+          variant = "us";
         };
-        emacs.enable = true;
-      };
 
-      browsers = {
-        enable = true;
-        default = "qutebrowser";
-        qutebrowser.enable = true;
-        firefox.enable = true;
-      };
-
-      terminals = {
-        enable = true;
-        alacritty.enable = true;
-      };
-
-      shells.fish = {
-        enable = true;
-        enableFlashyPrompt = true;
-        enableFileIcons = true;
+        packages = with pkgs; [
+          bitwarden
+          neofetch
+          coreutils
+          tldr
+          spotify
+          discord
+          teamspeak_client
+          signal-desktop
+          element-desktop
+        ];
       };
 
       programs = {
-        flameshot.enable = true;
-        comma.enable = true;
-        ranger.enable = true;
-        git.enable = true;
-        rofi.enable = true;
-        gpg.enable = true;
+        home-manager.enable = true;
+        git = {
+          userName = "gekoke";
+          userEmail = "gekoke@lazycantina.xyz";
+        };
+        mpv.enable = true;
+        rtorrent.enable = true;
       };
 
       services = {
-        mediakeys.enable = true;
-        email.enable = true;
-        picom.enable = true;
+        unclutter = {
+          enable = true;
+          timeout = 5;
+        };
       };
 
-      style.pointer.enable = true;
-    };
-  };
+      modules = {
+        windowmanagers.awesome.enable = true;
+
+        dev = {
+          nix.enable = true;
+          c.enable = true;
+          prolog.enable = true;
+          haskell.enable = true;
+          python.enable = true;
+          lua.enable = true;
+        };
+
+        editors = {
+          enable = true;
+          default = "nvim";
+          neovim = {
+            enable = true;
+            enableNix = true;
+            colorscheme = "nord";
+          };
+          emacs.enable = true;
+        };
+
+        browsers = {
+          enable = true;
+          default = "qutebrowser";
+          qutebrowser.enable = true;
+          firefox.enable = true;
+        };
+
+        terminals = {
+          enable = true;
+          alacritty.enable = true;
+        };
+
+        shells.fish = {
+          enable = true;
+          enableFlashyPrompt = true;
+          enableFileIcons = true;
+        };
+
+        programs = {
+          flameshot.enable = true;
+          comma.enable = true;
+          ranger.enable = true;
+          git.enable = true;
+          rofi.enable = true;
+          gpg.enable = true;
+        };
+
+        services = {
+          mediakeys.enable = true;
+          email.enable = true;
+          picom.enable = true;
+        };
+
+        style.pointer.enable = true;
+      };
+    }
+  ];
 }
