c3a8265b682a17fa18a5d9efdeeefcd3a8d7d4f3
gekoke <gekoke@lazycantina.xyz>
2023-01-24 01:38:56 +0200

    fix: dirvish keybinds

--- modules/hm/editors/emacs/features/dirvish/config.el
+++ modules/hm/editors/emacs/features/dirvish/config.el
@@ -1,4 +1,4 @@
-(use-package dirvish
+(use-package! dirvish
   :init (dirvish-override-dired-mode)
   :custom
   (dirvish-quick-access-entries ; It's a custom option, `setq' won't work
@@ -18,19 +18,18 @@
         "-l --almost-all --human-readable --group-directories-first --no-group")
   (map! :leader "r a" #'dirvish)
   (map! :leader "o p" #'dirvish-side)
-  (map! :map dirvish-mode-map
-        :localleader "k" #'dired-create-directory
-        :localleader "f" #'dired-create-empty-file
-        :n "h"   #'dired-up-directory
-        :n "q"   #'dirvish-quit
-        :n "l"   #'dired-find-file
-        :n "v"   #'dirvish-vc-menu
-        :n "a"   #'dirvish-quick-access
-        :n "f"   #'dirvish-file-info-menu
-        :n "s"   #'dirvish-quicksort
-        :n "C-i" #'dirvish-history-go-forward
-        :n "C-o" #'dirvish-history-go-backward
-        :n "y"   #'dirvish-yank-menu)
+  (map! :map dirvish-mode-map :localleader "k" #'dired-create-directory)
+  (map! :map dirvish-mode-map :localleader "f" #'dired-create-empty-file)
+  (map! :map dirvish-mode-map :ng "h"   #'dired-up-directory)
+  (map! :map dirvish-mode-map :ng "q"   #'dirvish-quit)
+  (map! :map dirvish-mode-map :ng "l"   #'dired-find-file)
+  (map! :map dirvish-mode-map :ng "v"   #'dirvish-vc-menu)
+  (map! :map dirvish-mode-map :ng "a"   #'dirvish-quick-access)
+  (map! :map dirvish-mode-map :ng "f"   #'dirvish-file-info-menu)
+  (map! :map dirvish-mode-map :ng "s"   #'dirvish-quicksort)
+  (map! :map dirvish-mode-map :ng "C-i" #'dirvish-history-go-forward)
+  (map! :map dirvish-mode-map :ng "C-o" #'dirvish-history-go-backward)
+  (map! :map dirvish-mode-map :ng "y"   #'dirvish-yank-menu)
   :bind                           ; Bind `dirvish|dirvish-side|dirvish-dwim' as you see fit
   (("C-c f" . dirvish-fd)
    :map dirvish-mode-map          ; Dirvish inherits `dired-mode-map'
