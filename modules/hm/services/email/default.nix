a9b1ba5f0958ed34ed27f0e14272f47f5556c5af
gekoke <gekoke@lazycantina.xyz>
2023-02-21 14:56:50 +0200

    refactor: prune email config until further notice

--- modules/hm/services/email/default.nix
+++ modules/hm/services/email/default.nix
@@ -12,15 +12,6 @@ in
   };
 
   config = mkIf cfg.enable {
-    programs = {
-      mu.enable = true;
-      mbsync.enable = true;
-      notmuch = {
-        enable = true;
-        hooks.preNew = "mbsync -a";
-      };
-    };
-
     accounts.email = {
       accounts = {
         school = {
@@ -31,37 +22,8 @@ in
           primary = true;
           realName = "gekoke";
           address = "gekoke@lazycantina.xyz";
-          smtp.host = "smtp.fastmail.com";
-          imap.host = "imap.fastmail.com";
-          userName = "mailbox@lazycantina.xyz";
-          passwordCommand = "cat ~/.mailpass";
-          mu.enable = true;
-          notmuch.enable = true;
-          mbsync = {
-            enable = true;
-            create = "maildir";
-          };
-          imapnotify = {
-            enable = true;
-            boxes = map (box: "personal/" + box) [
-              "Archive"
-              "Bills"
-              "Drafts"
-              "Inbox"
-              "Sent"
-              "Spam"
-              "Trash"
-            ];
-            onNotify = "${pkgs.isync}/bin/mbsync -a";
-            onNotifyPost = "touch ~/workedfile && ${pkgs.notmuch}/bin/notmuch new && ${pkgs.libnotify}/bin/notify-send 'New mail arrived'";
-          };
         };
       };
-    };
-
-    services = {
-      imapnotify.enable = true;
-      mbsync.enable = true;
     };
   };
 }
