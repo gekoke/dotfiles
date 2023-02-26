893f98747503a74f4ce3804e3b8acdb4bd12295f
gekoke <gekoke@lazycantina.xyz>
2023-02-26 04:31:38 +0200

    feat: make caps map to ctrl for XKB

--- hosts/luna/home.nix
+++ hosts/luna/home.nix
@@ -18,6 +18,9 @@ with lib;
       keyboard = {
         layout = "ee";
         variant = "us";
+        options = [
+          "caps:ctrl_modifier"
+        ];
       };
 
       packages = with pkgs; [
