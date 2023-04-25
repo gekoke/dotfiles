130712eca70a419b5ff583905072faf8ad4af635
gekoke <gekoke@lazycantina.xyz>
2023-04-25 10:58:11 +0300

    feat: strict blocking in Firefox

--- modules/hm/browsers/firefox/default.nix
+++ modules/hm/browsers/firefox/default.nix
@@ -27,16 +27,17 @@ in
           bitwarden
         ] ++ cfg.extraExtensions;
         settings = {
-          "media.hardwaremediakeys.enabled" = false;   # Disable media keys in Firefox - conflicts with Spotify etc. when audio is playing in browser
-          "browser.tabs.firefox-view"= false;          # Disable "Firefox View" button on home bar
-          "extensions.pocket.enabled" = false;         # Disable "pocket" button on home bar
-          "browser.uidensity" = 1;                     # Set UI density to compact
-          "browser.shell.checkDefaultBrowser" = false; # Don't show annoying warning about browser not being default
-          "browser.aboutconfig.showwarning" = false;   # Don't show warning when opening about:config
-          "signon.rememberSignons" = false;            # Don't use Firefox password manager
-          "browser.disableResetPrompt" = true;         # Disable "Looks like you haven't started Firefox in a while."
-          "browser.onboarding.enabled" = false;        # Disable "New to Firefox? Let's get started!" tour
-          "full-screen-api.warning.timeout" = 0;       # Disable "You're in fullscreen" message
+          "media.hardwaremediakeys.enabled" = false;     # Disable media keys in Firefox - conflicts with Spotify etc. when audio is playing in browser
+          "browser.tabs.firefox-view"= false;            # Disable "Firefox View" button on home bar
+          "extensions.pocket.enabled" = false;           # Disable "pocket" button on home bar
+          "browser.uidensity" = 1;                       # Set UI density to compact
+          "browser.shell.checkDefaultBrowser" = false;   # Don't show annoying warning about browser not being default
+          "browser.aboutconfig.showwarning" = false;     # Don't show warning when opening about:config
+          "signon.rememberSignons" = false;              # Don't use Firefox password manager
+          "browser.disableResetPrompt" = true;           # Disable "Looks like you haven't started Firefox in a while."
+          "browser.onboarding.enabled" = false;          # Disable "New to Firefox? Let's get started!" tour
+          "full-screen-api.warning.timeout" = 0;         # Disable "You're in fullscreen" message
+          "browser.contentblocking.category" = "strict"; # Set Firefox Tracking Protection to "Strict"
         };
       };
     };
