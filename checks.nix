12fea5357a55a4c42bda2e8758ba90537d601c81
Gregor Grigorjan <gregor@grigorjan.net>
2023-09-22 16:06:55 +0300

    feat(flake): add gitleaks

--- checks.nix
+++ checks.nix
@@ -4,7 +4,7 @@
     inputs.nix-pre-commit-hooks.flakeModule
   ];
 
-  perSystem = _: {
+  perSystem = { pkgs, ... }: {
     pre-commit = {
       check.enable = true;
 
@@ -15,6 +15,12 @@
           nixpkgs-fmt.enable = true;
           shellcheck.enable = true;
           statix.enable = true;
+          gitleaks = {
+            enable = true;
+            name = "gitleaks";
+            entry = "${pkgs.gitleaks}/bin/gitleaks protect --verbose --redact --staged";
+            pass_filenames = false;
+          };
         };
         settings.deadnix.edit = true;
       };
