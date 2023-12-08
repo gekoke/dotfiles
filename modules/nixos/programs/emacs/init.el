3ba1aead3920a9a1c0b7893eff0ced205fa605ee
Gregor Grigorjan <gregor@grigorjan.net>
2023-12-08 16:24:14 +0200

    tweak(emacs): font

--- modules/nixos/programs/emacs/init.el
+++ modules/nixos/programs/emacs/init.el
@@ -90,7 +90,7 @@
   :config
   (remember-last-theme-enable))
 
-(set-frame-font "FiraCode Nerd Font:pixelsize=15:weight=semi-bold" nil t)
+(set-frame-font "Iosevka Nerd Font:pixelsize=16:weight=semi-bold" nil t)
 
 (use-package ligature
   :config
