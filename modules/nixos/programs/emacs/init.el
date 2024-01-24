2b68444335b16c4897477741218e2a9869dfdfac
Gregor Grigorjan <gregor@grigorjan.net>
2024-01-24 17:23:18 +0200

    tweak(emacs): web-mode: auto-open tags

--- modules/nixos/programs/emacs/init.el
+++ modules/nixos/programs/emacs/init.el
@@ -644,7 +644,9 @@
   (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode)))
+  (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+  :custom
+  (web-mode-auto-open t))
 
 (use-package yaml-mode
   :mode ("\\.\\(yml\\|yaml\\)\\'"))
