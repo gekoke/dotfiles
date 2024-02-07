20b09a2b98adce55d43456138440ab9e2eb130d4
Gregor Grigorjan <gregor@grigorjan.net>
2024-02-07 14:55:37 +0200

    fix(emacs): django: web-mode-engines-alist

--- modules/nixos/programs/emacs/init.el
+++ modules/nixos/programs/emacs/init.el
@@ -677,7 +677,7 @@
   (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
   (add-hook 'web-mode-hook (lambda () (electric-pair-local-mode -1)))
   :custom
-  (web-mode-engines-alist '("django" . "\\\\.html\\\\."))
+  (web-mode-engines-alist '(("django" . "\\\\.html\\\\.")))
   (web-mode-enable-auto-pairing t)
   (web-mode-enable-auto-opening t)
   (web-mode-enable-auto-quoting t))
