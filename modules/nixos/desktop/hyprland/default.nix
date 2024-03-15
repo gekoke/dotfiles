93afb1029de58630271589891952f2010bb25f60
Gregor Grigorjan <gregor@grigorjan.net>
2024-03-15 14:29:50 +0200

    fix(hyprland): API breakage

--- modules/nixos/desktop/hyprland/default.nix
+++ modules/nixos/desktop/hyprland/default.nix
@@ -20,6 +20,18 @@ in
       xdg.portal = {
         enable = true;
 
+        # xdg-desktop-portal 1.17 reworked how portal implementations are loaded, you
+        # should either set `xdg.portal.config` or `xdg.portal.configPackages`
+        # to specify which portal backend to use for the requested interface.
+
+        # https://github.com/flatpak/xdg-desktop-portal/blob/1.18.1/doc/portals.conf.rst.in
+
+        # If you simply want to keep the behaviour in < 1.17, which uses the first
+        # portal implementation found in lexicographical order, use the following:
+
+        # xdg.portal.config.common.default = "*";
+        config.common.default = "*";
+
         # See https://github.com/NixOS/nixpkgs/issues/239886
         # Enable xdg-desktop-portal-gtk unless we already have it from Gnome DE
         extraPortals = lib.optionals (!config.services.xserver.desktopManager.gnome.enable) [ pkgs.xdg-desktop-portal-gtk ];
