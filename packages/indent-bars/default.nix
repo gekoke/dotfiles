41d4a30af7127c37914db6bf16a258d84d242b2f
Gregor Grigorjan <gregor@grigorjan.net>
2024-04-19 11:56:48 +0300

    chore: bump `indent-bars`

--- packages/indent-bars/default.nix
+++ packages/indent-bars/default.nix
@@ -5,13 +5,13 @@
 }:
 emacsPackages.trivialBuild rec {
   pname = "indent-bars";
-  version = "13cebfa";
+  version = "8826105";
 
   src = fetchFromGitHub {
     owner = "jdtsmith";
     repo = "indent-bars";
     rev = version;
-    hash = "sha256-CY51OLcdR99CFbFlXYoJAX8qvnucORjhzQeT5Kd+v8E=";
+    hash = "sha256-usa8xpg2dvXwFcQf8gKSZ+kOy6EGJ+e2mkvGAjEX66o=";
   };
 
   packageRequires = [
