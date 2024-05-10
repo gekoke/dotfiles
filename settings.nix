2bff5635b609b4c481165b19091e95591acd57a7
Gregor Grigorjan <gregor@grigorjan.net>
2024-05-11 01:03:04 +0300

    fix(nix): `trusted-public-keys` -> `extra-trusted-public-keys`

--- settings.nix
+++ settings.nix
@@ -3,7 +3,7 @@
     "https://nix-community.cachix.org"
     "https://hyprland.cachix.org"
   ];
-  trusted-public-keys = [
+  extra-trusted-public-keys = [
     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
   ];
