2aa9aeb08a48de1adcac9866d33fb28d7ecdce2a
Gregor Grigorjan <gregor@grigorjan.net>
2024-05-13 23:42:04 +0300

    tweak(emacs): font

--- modules/nixos/programs/emacs/init.el
+++ modules/nixos/programs/emacs/init.el
@@ -219,7 +219,7 @@
   :config
   (remember-last-theme-enable))
 
-(set-face-attribute 'default nil :family "Iosevka Term Nerd Font" :height 110 :weight 'bold)
+(set-face-attribute 'default nil :family "JetBrains Mono Nerd Font" :height 105 :weight 'normal)
 
 (use-package ligature
   :config
