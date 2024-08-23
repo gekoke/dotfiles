bb368965d277afd8f64c208df299a7bfac3f9aea
Gregor Grigorjan <gregor@grigorjan.net>
2024-08-23 19:05:58 +0300

    fix(emacs): dirvish: make `RET` binding more sensible

--- modules/nixos/programs/emacs/init.el
+++ modules/nixos/programs/emacs/init.el
@@ -487,7 +487,7 @@
     "s"    #'dirvish-quicksort  ; remapped `dired-sort-toggle-or-edit'
     "v"    #'dirvish-vc-menu    ; remapped `dired-view-file'
     "."    #'dired-omit-mode    ; remapped `dired-clean-directory'
-    "RET"  #'dirvish-subtree-toggle
+    "RET"  #'dirvish-subtree-toggle-or-open
     "<tab>" #'dirvish-subtree-toggle
     "M-l"  #'dirvish-ls-switches-menu
     "M-m"  #'dirvish-mark-menu
