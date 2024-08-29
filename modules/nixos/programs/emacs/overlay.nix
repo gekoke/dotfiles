91c1fe8c37fe29406a0e22ec42c53df1e466cc93
Gregor Grigorjan <gregor@grigorjan.net>
2024-08-29 23:26:43 +0300

    fix(emacs): dirvish: use maintained fork

--- modules/nixos/programs/emacs/overlay.nix
+++ modules/nixos/programs/emacs/overlay.nix
@@ -1,5 +1,15 @@
-_: _final: prev: {
+{ fetchFromGitHub }:
+_final: prev: {
   lsp-mode = prev.melpaPackages.lsp-mode.overrideAttrs (_: {
     LSP_USE_PLISTS = "true";
+  });
+
+  dirvish = prev.melpaPackages.dirvish.overrideAttrs (_: {
+    src = fetchFromGitHub {
+      owner = "hlissner";
+      repo = "dirvish";
+      rev = "5f04619";
+      hash = "sha256-VCTbhevhPMVVwBdkT0gdxcSOrWOs4IjdemdZJVDq9W4=";
+    };
   });
 }
