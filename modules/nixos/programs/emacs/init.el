e28ee3569234cdba6f8fe9cdc1972ac7fabc9703
Gregor Grigorjan <gregor.grigorjan@gamesglobal.com>
2024-12-19 16:02:18 +0200

    tweak(emacs): try the traditional Magit layout

--- modules/nixos/programs/emacs/init.el
+++ modules/nixos/programs/emacs/init.el
@@ -721,7 +721,6 @@
   (magit-no-confirm '(set-and-push stage-all-changes unstage-all-changes))
   (magit-bury-buffer-function #'magit-restore-window-configuration)
   (magit-revision-show-gravatars t)
-  (magit-display-buffer-function 'magit-display-buffer-fullframe-status-v1)
   :general
   (gg/leader
     "v" #'magit-status
