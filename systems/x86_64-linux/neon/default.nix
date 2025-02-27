0e69af51da345f9e0f8ed6f31737d6e9e86595be
Gregor Grigorjan <gregor@grigorjan.net>
2025-02-27 23:06:10 +0200

    chore(systems): `neon`: openssh: add `silicon` public key

--- systems/x86_64-linux/neon/default.nix
+++ systems/x86_64-linux/neon/default.nix
@@ -44,6 +44,7 @@
 
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDZjHdiGT2JDe/3tdEt5hNsOw6bOo0DEfGTkD4+7/ASs geko@carbon"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOnuNZ0JfFZ4sHUgatHZ+hE0qA+U7XX6m7ztTfmIrIgQ nixos@nixos"
   ];
 
   system.stateVersion = "25.05";
