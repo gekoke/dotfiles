a086bc5a8fd165b896174ddc2d18ca6dddb64659
Gregor Grigorjan <gregor.grigorjan@gamesglobal.com>
2025-03-07 14:23:05 +0200

    fix(emacs): Java: disable `emacs-lsp-booster`

--- modules/nixos/programs/emacs/init.el
+++ modules/nixos/programs/emacs/init.el
@@ -846,6 +846,7 @@
     (let ((orig-result (funcall old-fn cmd test?)))
       (if (and (not test?)                             ;; for check lsp-server-present?
                (not (file-remote-p default-directory)) ;; see lsp-resolve-final-command, it would add extra shell wrapper
+               (-none? (lambda (argv) (string-match ".*jdtls.*" argv)) orig-result) ;; disable for `jdtls' since it doesn't conform to the LSP spec and breaks `emacs-lsp-booster'
                lsp-use-plists
                (not (functionp 'json-rpc-connection))  ;; native json-rpc
                (executable-find "emacs-lsp-booster"))
