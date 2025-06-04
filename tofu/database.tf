28ff701237ac4c037c3cd4e1d9a538fd5455a551
Gregor Grigorjan <gregor.grigorjan@gamesglobal.com>
2025-06-04 10:35:46 +0300

    chore(tofu): db: destroy

--- tofu/database.tf
+++ tofu/database.tf
@@ -1,26 +1,3 @@
 provider "neon" {
   api_key = var.neon_api_key
 }
-
-resource "neon_project" "glucose" {
-  name      = "glucose"
-  region_id = "aws-eu-central-1"
-
-  branch {
-    name          = "main"
-    database_name = "glucosedb"
-    role_name     = "glucosedb_owner"
-  }
-}
-
-resource "neon_branch" "glucose_dev" {
-  name       = "dev"
-  project_id = neon_project.glucose.id
-  parent_id  = neon_project.glucose.default_branch_id
-}
-
-resource "neon_endpoint" "glucose_dev" {
-  type       = "read_write"
-  project_id = neon_project.glucose.id
-  branch_id  = neon_branch.glucose_dev.id
-}
