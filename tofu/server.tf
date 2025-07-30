a81a933a4021a33e42fb806510ec2d6cb69159ad
Gregor Grigorjan <gregor.grigorjan@gamesglobal.com>
2025-07-30 11:41:51 +0300

    Revert "chore(tofu): perf: test `build_on_remote = true`"
    This reverts commit b45e3d946121047e34243f368d07b8881cf16f14.

--- tofu/server.tf
+++ tofu/server.tf
@@ -30,5 +30,4 @@ module "neon_deploy" {
   nixos_partitioner_attr = ".#nixosConfigurations.neon.config.system.build.diskoScript"
   target_host            = hcloud_server.neon.ipv4_address
   instance_id            = hcloud_server.neon.id
-  build_on_remote        = true
 }
