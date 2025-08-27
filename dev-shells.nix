6f412a98bfb9ae85d03c03cea0ab8c3949822fb4
Gregor Grigorjan <gregor.grigorjan@gamesglobal.com>
2025-08-27 11:02:43 +0300

    refactor(dev-shells): remove unused

--- dev-shells.nix
+++ dev-shells.nix
@@ -38,29 +38,6 @@
             in
             [ openTofuWithPlugins ];
         };
-
-        playwright =
-          let
-            pkgs = inputs.nixpkgs-for-playwright.legacyPackages.${system};
-          in
-          pkgs.mkShellNoCC {
-            nativeBuildInputs = [
-              pkgs.playwright-driver.browsers
-            ];
-
-            shellHook = ''
-              export PLAYWRIGHT_BROWSERS_PATH=${pkgs.playwright-driver.browsers}
-              export PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS=true
-            '';
-          };
-
-        data = pkgs.mkShellNoCC {
-          packages = [
-            # See: https://github.com/NixOS/nixpkgs/issues/226107
-            # See: https://github.com/NixOS/nixpkgs/issues/417670
-            pkgs.dotnetCorePackages.dotnet_8.sdk
-          ];
-        };
       };
     };
 }
