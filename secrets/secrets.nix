836473722242604eab2aabe63daffcf5b127447b
Gregor Grigorjan <gregor.grigorjan@gamesglobal.com>
2025-08-29 13:49:52 +0300

    chore(secrets): reduce scope: `authinfo.age` -> `workStations`
    Only workstations should able to decrypt this value

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,6 +2,6 @@ let
   publicKey = import ../public-key.nix;
 in
 {
-  "authinfo.age".publicKeys = publicKey.ofAll.hosts;
+  "authinfo.age".publicKeys = publicKey.ofAll.workstations;
   "neon-nginx.htpasswd.age".publicKeys = publicKey.ofAll.hosts;
 }
