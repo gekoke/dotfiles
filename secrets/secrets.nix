589f03561124910ebb0fa15bd4bfe2ce8abbbd91
Gregor Grigorjan <gregor.grigorjan@gamesglobal.com>
2025-09-02 12:12:18 +0300

    tweak(secrets): use `--armor`
    Nicer diffs, nicer to read

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,8 @@
 let
   publicKey = import ../public-key.nix;
+  rules = {
+    "authinfo.age".publicKeys = publicKey.ofAll.workstations;
+    "neon-nginx.htpasswd.age".publicKeys = publicKey.ofAll.hosts;
+  };
 in
-{
-  "authinfo.age".publicKeys = publicKey.ofAll.workstations;
-  "neon-nginx.htpasswd.age".publicKeys = publicKey.ofAll.hosts;
-}
+builtins.mapAttrs (_: rule: rule // { armor = true; }) rules
