29c5a064ca99b91d9bbb12d03c1deafcd8b49355
Gregor Grigorjan <gregor.grigorjan@gamesglobal.com>
2025-09-04 10:14:06 +0300

    chore(tofu): database: destroy

--- tofu/database.tf
+++ tofu/database.tf
@@ -1,14 +1,3 @@
 provider "neon" {
   api_key = var.neon_api_key
 }
-
-resource "neon_project" "arugula" {
-  name      = "arugula"
-  region_id = "aws-eu-central-1"
-
-  branch {
-    name          = "main"
-    database_name = "arugula"
-    role_name     = "arugula_owner"
-  }
-}
