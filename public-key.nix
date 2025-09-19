423c2a4a598ae195f29785fdfe4297b96141f3a1
Gregor Grigorjan <gregor.grigorjan@gamesglobal.com>
2025-09-19 15:25:31 +0300

    chore(secrets): remove obsolete `TODO`
    Rekeyed in 8a795a1

--- public-key.nix
+++ public-key.nix
@@ -2,7 +2,6 @@ let
   workstations = {
     carbon = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDZjHdiGT2JDe/3tdEt5hNsOw6bOo0DEfGTkD4+7/ASs geko@carbon";
     silicon = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOnuNZ0JfFZ4sHUgatHZ+hE0qA+U7XX6m7ztTfmIrIgQ nixos@nixos";
-    # TODO: rekey
     hydrogen = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPNSy7ATWtt3KTv7UrEl3DAuL+iixSSF5xuGnJBAkaoG geko@hydrogen";
   };
   vms = {
