7e4471bc12220201012c3ce86e3d7b8e599b0b20
Gregor Grigorjan <gregor@grigorjan.net>
2025-09-22 18:36:23 +0300

    chore(substituters): add Cachix cache

--- flake.nix
+++ flake.nix
@@ -3,10 +3,12 @@ rec {
     extra-substituters = [
       "https://nix-community.cachix.org"
       "https://hyprland.cachix.org"
+      "https://gekoke-dotfiles.cachix.org"
     ];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      "gekoke-dotfiles.cachix.org-1:mED8HYGwRLMcDvi54a/Qxl5LshQtOXRt3rlXHw4GkDw="
     ];
   };
 
