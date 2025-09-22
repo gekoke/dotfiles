d675a7cd7a5660dace077648c6aedc269e5b45c1
Gregor Grigorjan <gregor@grigorjan.net>
2025-09-22 20:23:45 +0300

    fix(secrets): reduce scope

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -4,11 +4,14 @@ let
     "authinfo.age".publicKeys = publicKey.ofAll.workstations;
 
     # Deploy secrets
-    "aws-secret-key.age".publicKeys = publicKey.ofAll.hosts ++ [ publicKey.for.githubActions ];
-    "cachix-auth-token.age".publicKeys = publicKey.ofAll.hosts ++ [ publicKey.for.githubActions ];
-    "cloudflare-api-token.age".publicKeys = publicKey.ofAll.hosts ++ [ publicKey.for.githubActions ];
-    "hcloud-token.age".publicKeys = publicKey.ofAll.hosts ++ [ publicKey.for.githubActions ];
-    "neon-api-key.age".publicKeys = publicKey.ofAll.hosts ++ [ publicKey.for.githubActions ];
+    "aws-secret-key.age".publicKeys = publicKey.ofAll.workstations ++ [ publicKey.for.githubActions ];
+    "cachix-auth-token.age".publicKeys = publicKey.ofAll.workstations ++ [
+      publicKey.for.githubActions
+    ];
+    "cloudflare-api-token.age".publicKeys = publicKey.ofAll.workstations ++ [
+      publicKey.for.githubActions
+    ];
+    "neon-api-key.age".publicKeys = publicKey.ofAll.workstations ++ [ publicKey.for.githubActions ];
   };
 in
 builtins.mapAttrs (_: rule: rule // { armor = true; }) rules
