d0807ebfcba1093cfe5e268b7c1ed3d1c06133ca
Gregor Grigorjan <gregor@grigorjan.net>
2025-09-22 23:57:10 +0300

    chore(tofu): GitHub: import: repository: `dotfiles`

--- tofu/github.tf
+++ tofu/github.tf
@@ -2,6 +2,17 @@ provider "github" {
   token = var.github_token
 }
 
+resource "github_repository" "dotfiles" {
+  name          = "dotfiles"
+  description   = "My NixOS system configurations"
+  has_issues    = true
+  has_downloads = false
+  has_projects  = false
+  has_wiki      = false
+
+  allow_auto_merge = true
+}
+
 resource "github_repository" "resume" {
   name          = "resume"
   homepage_url  = "https://www.grigorjan.net/resume.pdf"
