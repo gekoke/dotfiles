5634b0689eba03e54110f8afd0e63dfbef4f3145
Gregor Grigorjan <gregor.grigorjan@gamesglobal.com>
2025-09-23 10:15:06 +0300

    style(tofu): apply `tflint` suggestions

---
+++ tofu/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_version = "~>1.10"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>6.8"
+    }
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~>5.8"
+    }
+    github = {
+      source  = "integrations/github"
+      version = "~>6.6"
+    }
+    hcloud = {
+      source  = "hetznercloud/hcloud"
+      version = "~>1.52"
+    }
+    neon = {
+      source  = "kislerdm/neon"
+      version = "~>0.9"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~>4.1"
+    }
+  }
+}
