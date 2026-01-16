3642a050fe057ad74ace05d00b6e041ca0648bd7
Gregor Grigorjan <gregor@grigorjan.net>
2026-01-16 16:48:58 +0200

    refactor(packages): `http_proxy_connect_module`: collapse code

--- packages/http_proxy_connect_module/default.nix
+++ packages/http_proxy_connect_module/default.nix
@@ -4,25 +4,18 @@
 }:
 # FIXME: remove when https://github.com/NixOS/nixpkgs/pull/351880
 # is merged
-pkgs.stdenv.mkDerivation {
+pkgs.stdenv.mkDerivation rec {
   pname = "http_proxy_connect_module";
   version = "4f0b6c2297862148c59a0d585d6c46ccb7e58a39";
+
   src = pkgs.fetchFromGitHub {
     owner = "chobits";
     repo = "ngx_http_proxy_connect_module";
-    rev = "4f0b6c2297862148c59a0d585d6c46ccb7e58a39";
+    rev = version;
     sha256 = "sha256-Yob2Z+a3ex3Ji6Zz8J0peOYnKpYn5PlC9KsQNcHCL9o=";
   };
-  patches = [
-    "${
-      pkgs.fetchFromGitHub {
-        owner = "chobits";
-        repo = "ngx_http_proxy_connect_module";
-        rev = "4f0b6c2297862148c59a0d585d6c46ccb7e58a39";
-        sha256 = "sha256-Yob2Z+a3ex3Ji6Zz8J0peOYnKpYn5PlC9KsQNcHCL9o=";
-      }
-    }/patch/proxy_connect_rewrite_102101.patch"
-  ];
+
+  patches = [ "${src}/patch/proxy_connect_rewrite_102101.patch" ];
 
   meta = {
     license = [ lib.licenses.bsd2 ];
