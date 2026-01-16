3d1ac45b1c8be8337ac0dc4d07c3f03a96ecd9ef
Gregor Grigorjan <gregor@grigorjan.net>
2026-01-16 14:37:55 +0200

    miasma-theme: 7651717 -> 1.6.6-unstable-2026-01-13
    Diff: https://github.com/daut/miasma-theme.el/compare/7651717...998aa50769722f1a92f99124d76a1689c2d9eaf1

--- packages/miasma-theme/default.nix
+++ packages/miasma-theme/default.nix
@@ -6,13 +6,13 @@
 }:
 emacsPackages.trivialBuild rec {
   pname = "miasma-theme";
-  version = "7651717";
+  version = "998aa50769722f1a92f99124d76a1689c2d9eaf1";
 
   src = fetchFromGitHub {
     owner = "daut";
     repo = "miasma-theme.el";
     rev = version;
-    hash = "sha256-0k7yFtyRKcMGniTil5AgiFixSyqm4mrhR7rvoHBv+AE=";
+    hash = "sha256-O1Vdz+o2BvI6Zlws5blwx/BMrXlDX17HkxxI4AQiGv4=";
   };
 
   meta = {
