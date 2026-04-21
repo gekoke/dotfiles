04d2f4064c7bcf8e73f700c971b541b098caf83b
Gregor Grigorjan <gregor@grigorjan.net>
2026-04-21 16:56:00 +0300

    chore(tofu): remove deprecated argument
    Fixes:
    
    ```
    ╷
    │ Warning: Argument is deprecated
    │
    │   with github_repository.dotfiles,
    │   on github.tf line 9, in resource "github_repository" "dotfiles":
    │    9:   has_downloads = false
    │
    │ This attribute is no longer in use, but it hasn't been removed yet. It will
    │ be removed in a future version. See
    │ https://github.com/orgs/community/discussions/102145#discussioncomment-8351756
    ```

--- tofu/github.tf
+++ tofu/github.tf
@@ -3,21 +3,19 @@ provider "github" {
 }
 
 resource "github_repository" "dotfiles" {
-  name          = "dotfiles"
-  description   = "My NixOS system configurations"
-  has_issues    = true
-  has_downloads = false
-  has_projects  = false
-  has_wiki      = false
+  name         = "dotfiles"
+  description  = "My NixOS system configurations"
+  has_issues   = true
+  has_projects = false
+  has_wiki     = false
 
   allow_auto_merge = true
 }
 
 resource "github_repository" "resume" {
-  name          = "resume"
-  homepage_url  = "https://www.grigorjan.net/resume.pdf"
-  has_issues    = true
-  has_downloads = false
-  has_projects  = false
-  has_wiki      = false
+  name         = "resume"
+  homepage_url = "https://www.grigorjan.net/resume.pdf"
+  has_issues   = true
+  has_projects = false
+  has_wiki     = false
 }
